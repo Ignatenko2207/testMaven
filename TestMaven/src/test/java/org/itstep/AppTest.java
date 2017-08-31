42d35931eba3a8b8933cb1163d89def95ed638d3
Alex <ignatenko2207@gmail.com>
2017-08-31 20:22:39 +0300

    First commit

---
+++ TestMaven/src/test/java/org/itstep/AppTest.java
@@ -0,0 +1,14 @@
+package org.itstep;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest {
+    
+   
+    public void testApp()
+    {
+
+    }
+}
